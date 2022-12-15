50445a32ff8944c51a887d43104319af17de23e2
Santhosh-Gowda23 <Santhosh.b@publicissapient.com>
2022-12-15 12:42:00 +0530

    Revert "first commit"
    This reverts commit 69a342ab10d7dd55352f672583a7aa0bdaa704f2.

--- home/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-// import "@module-federation/nextjs-mf/lib/include-defaults";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- home/pages/catalogOnHome.js
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const SampleComponent = dynamic(() => import("catalog/pages/catalogOnShop"), {
-  ssr: false,
-});
-
-export default SampleComponent;
